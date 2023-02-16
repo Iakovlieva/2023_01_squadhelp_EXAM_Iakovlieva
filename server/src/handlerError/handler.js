7acc6e3edf3691d71226c13f8566180e566b741e
Iakovlieva <iakovlieva.so@gmail.com>
2023-02-16 10:18:26 +0200

    task12_Loger_done

--- server/src/handlerError/handler.js
+++ server/src/handlerError/handler.js
@@ -1,3 +1,33 @@
+const { Writable } = require('stream');
+const fs = require('fs');
+const path = require('path');
+
+
+const fileloging = async (filepath, text) => {
+
+  fs.writeFileSync(
+    filepath,
+    `${text}\n`,
+    { encoding: "utf-8", flag: "a+" }
+  );
+
+    /*const nodeWritable = fs.createWriteStream(
+      filepath,
+      { encoding: "utf-8" }
+    );
+
+    const webWritableStream = Writable.toWeb(nodeWritable); //???
+    const writer = webWritableStream.getWriter();
+
+    try {
+      await writer.write(`${text}\n`);  
+      await writer.close();
+    } finally {
+      writer.releaseLock()
+    }
+    */
+}
+
 module.exports = (err, req, res, next) => {
   console.log(err);
   if (err.message ===
@@ -7,9 +37,38 @@ module.exports = (err, req, res, next) => {
     err.message = 'Not Enough money';
     err.code = 406;
   }
+        /*
+        if (!err.message || !err.code) {
+          err.message = 'Server Error';
+          err.code = 500;
+          res.status(500).send('Server Error');
+        } else {
+          res.status(err.code).send(err.message);
+        }
+        */
   if (!err.message || !err.code) {
-    res.status(500).send('Server Error');
-  } else {
-    res.status(err.code).send(err.message);
+    err.message = 'Server Error';
+    err.code = 500;
   }
+
+  const env = process.env.NODE_ENV || 'development';
+  const devFilePath = path.resolve(__dirname, '../../public/logfiles');  
+  const filePath = env === 'production'
+  ? '/var/www/html/logfiles/'
+  : devFilePath;
+
+  if (!fs.existsSync(filePath)) {
+    fs.mkdirSync(filePath, {
+      recursive: true,
+    });
+  } 
+
+  const logFileName = `${filePath}/logfile.txt`;
+
+  const errLogText=`{message: "${err.message}", time: ${Date.parse(new Date())}, code: ${err.code}, stackTrace: {${err.stack}}`;    
+
+  fileloging(logFileName, errLogText);
+
+  res.status(err.code).send(err.message);
+  
 };
